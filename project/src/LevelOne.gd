81afdbd8113a226b254e32b412b1102c518f7029
Jake Dibble <jdibble@bsu.edu>
2020-10-27 14:54:07 -0400

    Add scrolling background & asteroid hazard

--- project/src/LevelOne.gd
+++ project/src/LevelOne.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+const SPEED := 4
+
+onready var _background := $ParallaxBackground/ParallaxLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	if _background.position.y >= 800:
+		_background.motion_offset.y = 0
+	_background.motion_offset.y += SPEED
+		
--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,25 +1,27 @@
 extends KinematicBody2D
 
 export var velocity := 600
-
+export var sideways_velocity := 500
 var _current_pos := Vector2()
 
 func _ready():
 	$ShipSprite.play("normal")
 
 func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		print("firing!")
 	if Input.is_action_pressed("move_left") and position.x > -479:
-		_current_pos.x -= velocity * delta
+		_current_pos.x -= sideways_velocity * delta
 		$ShipSprite.play("turning")
 		$ShipSprite.flip_h = false
 	if Input.is_action_pressed("move_right") and position.x < 509:
-		_current_pos.x += velocity * delta
+		_current_pos.x += sideways_velocity * delta
 		$ShipSprite.play("turning")
 		$ShipSprite.flip_h = true
 	if Input.is_action_pressed("move_up"):
 		_current_pos.y -= velocity * delta
-	if Input.is_action_just_pressed("move_down"):
-		_current_pos.y += -velocity * delta
+	if Input.is_action_pressed("move_down"):
+		_current_pos.y += velocity * delta
 	if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
 		$ShipSprite.play("normal")
 	position = _current_pos
