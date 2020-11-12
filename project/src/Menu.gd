7995e8a5f8cfaf848b9fef31417b1d2cea2db31d
Jake Dibble <jdibble@bsu.edu>
2020-11-12 01:56:47 -0500

    Add menu sprite animations

--- project/src/Menu.gd
+++ project/src/Menu.gd
@@ -2,13 +2,28 @@
 # button press
 extends CanvasLayer
 
+const SPEED := 200
+
 onready var _gameplay_scene = load("res://src/LevelOne/LevelOne.tscn")
 
 func _ready():
-	$ShipSprite/ShipExhaust.play("active")
 	$MenuMusic.play()
+	$BossShip/Exhaust1.play("active")
+	$BossShip/Exhaust2.play("active")
 
 
 func _on_PlayButton_pressed():
 	queue_free()
 	get_tree().get_root().add_child(_gameplay_scene.instance())
+
+
+func _on_SpriteFlyByTimer_timeout():
+	
+	$AnimationPlayer.play("flyby")
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("boss_ship")
+	yield(get_node("AnimationPlayer"),"animation_finished")
+	pass
+
+
+
