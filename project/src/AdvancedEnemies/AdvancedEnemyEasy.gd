369797ba3ec0c87df7757d2217832d30d87aceaa
Jake Dibble <jdibble@bsu.edu>
2020-11-15 17:45:33 -0500

    Add single photon projectile

--- project/src/AdvancedEnemies/AdvancedEnemyEasy.gd
+++ project/src/AdvancedEnemies/AdvancedEnemyEasy.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
+const SPEED := 80
 
 func _ready():
 	$Exhaust1.play("active")
 	$Exhaust2.play("active")
 
 
+func _process(delta):
+	position += transform.y * SPEED * delta
+	if position.y >= 800:
+		queue_free()
---
+++ project/src/Projectiles/PhotonSingleBullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const SPEED := 240
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	position += transform.y * SPEED * delta
+	if position.y >= 800:
+		queue_free()
+
+
+func _on_PhotonSingleBullet_area_entered(area):
+	if area.is_in_group("player"):
+		queue_free()
