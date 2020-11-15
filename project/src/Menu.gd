db86545bc30f8804cef763fc1ac54eb1a73b450f
Jake Dibble <jdibble@bsu.edu>
2020-11-15 16:52:12 -0500

    Create options panel

--- project/src/Menu.gd
+++ project/src/Menu.gd
@@ -4,26 +4,41 @@ extends CanvasLayer
 
 const SPEED := 200
 
-onready var _gameplay_scene = load("res://src/LevelOne/LevelOne.tscn")
+onready var _level_one_scene = load("res://src/LevelOne/LevelOne.tscn")
 
 func _ready():
 	$MenuMusic.play()
 	$BossShip/Exhaust1.play("active")
 	$BossShip/Exhaust2.play("active")
+	$LevelSelectPanel.hide()
+	$OptionsPanel.hide()
 
 
 func _on_PlayButton_pressed():
-	queue_free()
-	get_tree().get_root().add_child(_gameplay_scene.instance())
+	$LevelSelectPanel.show()
 
 
 func _on_SpriteFlyByTimer_timeout():
-	
 	$AnimationPlayer.play("flyby")
-	yield(get_node("AnimationPlayer"), "animation_finished")
-	$AnimationPlayer.play("boss_ship")
+	#yield(get_node("AnimationPlayer"), "animation_finished")
+	#$AnimationPlayer.play("boss_ship")
 	yield(get_node("AnimationPlayer"),"animation_finished")
 	pass
 
 
 
+func _on_LevelOneButton_pressed():
+	queue_free()
+	get_tree().get_root().add_child(_level_one_scene.instance())
+
+
+func _on_PlayBackButton_pressed():
+	$LevelSelectPanel.hide()
+
+
+func _on_OptionsButton_pressed():
+	$OptionsPanel.show()
+
+
+func _on_OptionsBackButton_pressed():
+	$OptionsPanel.hide()
