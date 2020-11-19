aa61ad2a187ab4266d7a306ac2b5f2f4516cffca
Jake Dibble <jdibble@bsu.edu>
2020-11-18 21:24:06 -0500

    Allow defeated enemy to drop powerup

--- project/src/PowerUps/LifePowerUp.gd
+++ project/src/PowerUps/LifePowerUp.gd
@@ -1,8 +1,15 @@
-# Detects when player enters powerup area, plays collect sound
-# and deletes itself
+# Handles powerup movement, detects when player enters powerup area, 
+# plays collect sound and deletes itself
 extends Area2D
+
+const SPEED := 100
 
 func _on_LifePowerUp_area_entered(area):
 	if area.is_in_group("player"):
 		$PickupSound.play()
+		queue_free()
+		
+func _process(delta):
+	position += transform.y * SPEED * delta
+	if position.y >= 800:
 		queue_free()
--- project/src/StandardEnemies/StandardEnemyEasy.gd
+++ project/src/StandardEnemies/StandardEnemyEasy.gd
@@ -4,15 +4,17 @@ extends KinematicBody2D
 const SPEED := 60
 
 export (PackedScene) var Bullet 
+export (PackedScene) var HealthPowerup
+export (PackedScene) var AttackPowerup
 
 var _initial_fire_delay := randi()%6+1
 var _fire_delay := randi()%8+2
-var _powerup_drop_chance := randi()%4+1
+var _powerup_drop_chance := randi()%3+1
 var _time_elapsed := 0.0
 
 func _ready():
 	$AnimatedSprite.play("normal")
-
+	
 	
 func _process(delta):
 	_time_elapsed += delta
@@ -36,6 +38,11 @@ func _on_HitBox_area_entered(area):
 		$AnimatedSprite.play("destroyed")
 		$DestroyedSound.play()
 		$Muzzle/Sprite.hide()
+		if _powerup_drop_chance == 2:
+			var power_up = HealthPowerup.instance()
+			var root_attach = get_tree().get_root().get_node("LevelOne")
+			root_attach.add_child(power_up)
+			power_up.position = self.position
 		var timer = Timer.new()
 		timer.set_wait_time(0.3)
 		add_child(timer)
